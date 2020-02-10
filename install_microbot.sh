b5b59e9c41df2e99f18bb3441ffcc5f77543b0b0
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2020-02-11 07:55:58 +0900

    remove: ydlidar and roboclaw from client PC

--- install_microbot.sh
+++ install_microbot.sh
@@ -3,15 +3,9 @@
 sudo /usr/bin/python2 -m pip install pycrc -U
 cd ~/catkin_ws/src
 git clone https://github.com/KMiyawaki/oit_navigation_microbot_01.git
-git clone https://github.com/KMiyawaki/oit_roboclaw_driver.git
 git clone https://github.com/KMiyawaki/my_microbot_apps.git
-git clone https://github.com/YDLIDAR/ydlidar_ros.git
 git clone https://github.com/MoriKen254/timed_roslaunch.git
-cd ~/catkin_ws/src/ydlidar_ros
-git checkout refs/tags/1.4.1
 cd ~/catkin_ws/src/timed_roslaunch
 git checkout -b melodic-devel origin/melodic-devel
 cd ~/catkin_ws
 catkin_make
-cd ~/catkin_ws/src/ydlidar_ros/startup
-sudo sh initenv.sh 
