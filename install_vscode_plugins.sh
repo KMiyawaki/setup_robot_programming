41c1a6b7890f9146d214f17290654e6a798c90f8
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2020-03-21 15:38:21 +0900

    add: vscode plugin install flag

--- install_vscode_plugins.sh
+++ install_vscode_plugins.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-code --install-extension DavidAnson.vscode-markdownlint
-code --install-extension DotJoshJohnson.xml
-code --install-extension eamodio.gitlens
-code --install-extension Kelvin.vscode-sshfs
-code --install-extension ms-iot.vscode-ros
-code --install-extension ms-python.python
-code --install-extension ms-vscode.cpptools
-code --install-extension shakram02.bash-beautify
-code --install-extension yzhang.markdown-all-in-one
+code --install-extension DavidAnson.vscode-markdownlint --force
+code --install-extension DotJoshJohnson.xml --force
+code --install-extension eamodio.gitlens --force
+code --install-extension Kelvin.vscode-sshfs --force
+code --install-extension ms-iot.vscode-ros --force
+code --install-extension ms-python.python --force
+code --install-extension ms-vscode.cpptools --force
+code --install-extension shakram02.bash-beautify --force
+code --install-extension yzhang.markdown-all-in-one --force
