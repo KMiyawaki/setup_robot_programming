48164ff16d68bea432a226352281a4f34a3f472e
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2020-05-09 21:39:04 +0900

    fix: python essential packages

--- install_ros_web.sh
+++ install_ros_web.sh
@@ -3,6 +3,8 @@ sudo apt install ros-melodic-roswww -y
 sudo apt install ros-melodic-rosbridge-suite -y
 sudo apt install ros-melodic-web-video-server -y
 sudo apt install ros-melodic-tf2-web-republisher -y
+sudo /usr/bin/python2 -m pip install tornado -U
+sudo /usr/bin/python2 -m pip install pymongo -U
 cd ~/catkin_ws/src
 git clone https://github.com/GT-RAIL/robot_pose_publisher.git
 cd ..
