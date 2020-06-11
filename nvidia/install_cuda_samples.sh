ab31e3534a66a7f5d0269e7d518b8b69a36ddb32
Kenzaburo Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2020-06-11 17:54:18 +0900

    fix: wrong path name

--- nvidia/install_cuda_samples.sh
+++ nvidia/install_cuda_samples.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/local/cuda/bin/cuda-install-samples-11.0.sh ~/cuda-11.0_samples
-cd ~/cuda-11.0_samples
+/usr/local/cuda/bin/cuda-install-samples-11.0.sh ~
+cd ~/NVIDIA_CUDA-11.0_Samples
 # https://github.com/NVIDIA/cuda-samples/issues/22
 make -k
