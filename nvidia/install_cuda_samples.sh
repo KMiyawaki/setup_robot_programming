2fe0ad48ffe66f841010bc67b5c6b01e6c7bbf1a
Kenzaburo Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2020-06-11 17:52:39 +0900

    update: for cuda11

--- nvidia/install_cuda_samples.sh
+++ nvidia/install_cuda_samples.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/local/cuda-10.2/bin/cuda-install-samples-10.2.sh ~/cuda-10.2_samples
-cd ~/cuda-10.2_samples
+/usr/local/cuda/bin/cuda-install-samples-11.0.sh ~/cuda-11.0_samples
+cd ~/cuda-11.0_samples
 # https://github.com/NVIDIA/cuda-samples/issues/22
 make -k
