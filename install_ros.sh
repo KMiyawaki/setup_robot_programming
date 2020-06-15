d5cc60fb5817f3f05b360e1af1cd5caddbdf7327
oit-trial <robocup_teachers_hirakata@ml.is.oit.ac.jp>
2020-06-15 15:16:11 +0900

    fix: install command rosdep

--- install_ros.sh
+++ install_ros.sh
@@ -3,14 +3,13 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
 sudo apt install ros-melodic-desktop-full -y
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-tools -y
 sudo rosdep init
 rosdep update
 if ! grep -q /opt/ros/melodic/setup.bash ~/.bashrc; then
   echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
   source ~/.bashrc
 fi
-sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential -y
-sudo apt install python-catkin-tools -y
 # ROS packages
 sudo apt install ros-melodic-rosemacs -y
 ./setup_emacs.sh
