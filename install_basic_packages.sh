ae9a9b51bae3e6fa514cbe37484392fec4c51083
KMiyawaki <5770105+KMiyawaki@users.noreply.github.com>
2021-01-27 15:04:05 +0900

    add: zip unzip

--- install_basic_packages.sh
+++ install_basic_packages.sh
@@ -8,5 +8,6 @@ sudo apt install net-tools -y
 sudo apt install openssh-server -y
 sudo apt install python-pip -y
 sudo apt install python3-pip -y 
+sudo apt install zip unzip -y
 sudo apt install gparted -y 
 sudo apt install x11-apps x11-utils x11-xserver-utils dbus-x11 -y
