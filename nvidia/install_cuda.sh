8267a30988477376f9880206d1607ff102f8bcb1
Kenzaburo Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2021-07-30 12:30:16 +0900

    update: cuda version

--- nvidia/install_cuda.sh
+++ nvidia/install_cuda.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-sudo update-pciids
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.168-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1804_10.1.168-1_amd64.deb
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update
-sudo apt-get install cuda cuda-drivers
-rm cuda-repo-ubuntu1804_10.1.168-1_amd64.deb
-sudo reboot
+sudo apt-get -y install cuda
