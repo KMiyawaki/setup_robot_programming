7eeebdfeaf473a5d7507b123b7b1a711ec14ae69
Kenzaburo Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2021-08-09 13:59:52 +0900

    fix: wrong replace

--- set_japanese_apt.sh
+++ set_japanese_apt.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-sudo sed -i 's@archive.ubuntu.com@ftp.jaist.ac.jp/pub/Linux@g' /etc/apt/sources.list
+BACK_NAME=`date '+%Y%m%d_%H%M%S'`
+BACK_NAME="/etc/apt/sources.list.${BACK_NAME}"
+sudo cp /etc/apt/sources.list ${BACK_NAME}
+sudo sed -i 's@us.archive.ubuntu.com@ftp.jaist.ac.jp/pub/Linux@g' /etc/apt/sources.list
