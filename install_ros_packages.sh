d125f2d216bb5c6004e8ffca07ba5e2be7b22a16
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2022-12-10 08:46:42 +0900

    add: control-msgs

--- install_ros_packages.sh
+++ install_ros_packages.sh
@@ -3,7 +3,7 @@
 function main(){
     local -r TARGET_ROS=`./get_ros_distoro.sh`
     # ROS packages
-    local -r PACKAGES="cv-camera \
+    local -r PACKAGES="cv-camera control-msgs \
     image-publisher image-transport image-transport-plugins \
     joint-state-publisher-gui joy \
     key-teleop \
