6655c357be1ed52a17228b277320f140e5fa34d0
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2022-12-15 10:03:05 +0900

    fix: curl install

--- install_ros_key.sh
+++ install_ros_key.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function main(){
+    sudo apt-get install -y --no-install-recommends curl
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
     curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 }
