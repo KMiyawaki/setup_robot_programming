53db703d5616fb42c5733573e5485653412b0844
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2023-02-04 03:04:15 +0900

    change: remove python2 install for noetic

--- install_python_packages.sh
+++ install_python_packages.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
 function main(){
-    sudo pip install --upgrade pip
-    sudo pip3 install --upgrade pip
-    local -r PACKAGES="pycodestyle pylint autopep8 autoflake"
-    
-    for p in ${PACKAGES}; do
-        sudo /usr/bin/python2 -m pip install ${p}
-        sudo /usr/bin/python3 -m pip install ${p}
-    done
+    local -r VERSION_ID=`cat /etc/os-release | grep VERSION_ID`
+    if [[ $VERSION_ID =~ "16." ]] || [[ $VERSION_ID =~ "18." ]]; then
+        sudo pip install --upgrade pip
+        sudo pip3 install --upgrade pip
+        local -r PACKAGES="pycodestyle pylint autopep8 autoflake"
+        
+        for p in ${PACKAGES}; do
+            sudo /usr/bin/python2 -m pip install ${p}
+            sudo /usr/bin/python3 -m pip install ${p}
+        done
+    elif [[ $VERSION_ID =~ "20." ]] || [[ $VERSION_ID =~ "22." ]]; then
+        sudo pip3 install --upgrade pip
+        local -r PACKAGES="pycodestyle pylint autopep8 autoflake"
+        
+        for p in ${PACKAGES}; do
+            sudo /usr/bin/python3 -m pip install ${p}
+        done
+    else
+        echo "*** ${VERSION_ID} is not supported ***"
+    fi
 }
 
 main "$@"
