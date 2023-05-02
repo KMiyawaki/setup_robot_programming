19dc0b9c893e66519e88123c4816a7d23166962f
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2023-05-02 19:25:19 +0900

    fix: cuda gpg

---
+++ get_ubuntu_version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function main(){
+    local -r VERSION=`cat /etc/os-release | grep VERSION_ID | sed 's/^.*"\(.*\)".*$/\1/'`
+    
+    if [ $# != 0 ]; then
+        echo ${VERSION}
+    else
+        echo ${VERSION/./}
+    fi
+}
+
+main "$@"
