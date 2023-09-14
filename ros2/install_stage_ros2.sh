7908774d382156399f8cf8d195ac83f4f7caaa07
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2023-09-14 16:37:47 +0900

    fix: stage for ros2 installer

--- ros2/install_stage_ros2.sh
+++ ros2/install_stage_ros2.sh
@@ -2,19 +2,19 @@
 
 function main(){
     cd "$(dirname "$0")"
-    local -r WS="${HOME}/ros2_ws"
-    sudo install -y --no-install-recommends libfltk1.3-dev
-    git clone https://github.com/rtv/Stage.git ~/Stage
-    cd ~/Stage
-    mkdir build
-    cd build/
-    cmake ..
-    make install
-    cd ${WS}/src/
-    git clone https://github.com/n0nzzz/stage_ros2.git
-    cd ${WS}
-    # Error
-    colcon build --symlink-install
+    local -r TARGET_ROS=`./get_suitable_ros2.sh`
+    if [[ "${TARGET_ROS}" =~ "humble" ]]; then
+        local -r WS="${HOME}/ros2_ws"
+        sudo apt-get install -y --no-install-recommends git cmake g++ libjpeg8-dev libpng-dev libglu1-mesa-dev libltdl-dev libfltk1.1-dev
+        cd ${WS}/src
+        git clone --branch ros2 https://github.com/tuw-robotics/Stage.git
+        git clone --branch humble https://github.com/tuw-robotics/stage_ros2.git
+        cd ${WS}
+        colcon build --symlink-install --cmake-args -DOpenGL_GL_PREFERENCE=LEGACY
+        colcon build --symlink-install --packages-select stage_ros2
+    else
+        echo "*** Stage for ${TARGET_ROS} is not supported ***"
+    fi
 }
 
 main "$@"
