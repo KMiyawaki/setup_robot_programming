df057bfbe446f2a5d22ccc210f7fe64847775609
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2023-10-01 08:49:01 +0900

    fix: ros1 key installer
    add sudo

--- ros1/install_ros1_key.sh
+++ ros1/install_ros1_key.sh
@@ -3,7 +3,7 @@
 function main(){
     sudo apt-get install -y --no-install-recommends curl
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-    sudo curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
+    sudo curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 }
 
 main "$@"
