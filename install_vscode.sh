abc1e38a98c5f2691df131869d17176c00b02c7f
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2023-11-13 16:28:28 +0900

    change: install vscode from apt

--- install_vscode.sh
+++ install_vscode.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 function main(){
-    wget -O code.deb https://go.microsoft.com/fwlink/?LinkID=760868
-    sudo dpkg -i code.deb
-    rm -f code.deb
+    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+    local -r UNAME=`uname -m`
+    local ARCH=""
+    if [[ $UNAME =~ "x86_64" ]]; then
+        ARCH="amd64"
+    elif [[ $UNAME =~ "aarch64" ]]; then
+        ARCH="arm64"
+    else
+        echo "${UNAME} is not supported ***"
+        exit 1
+    fi
+    echo "Install VSCode for ${ARCH}"
+    sudo add-apt-repository "deb [arch=${ARCH}] https://packages.microsoft.com/repos/vscode stable main"
+    sudo apt-get update
+    sudo apt-get install -y code 
 }
 
 main "$@"
