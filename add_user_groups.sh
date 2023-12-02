3e0d257797fbbdac80e35954f6d607c98883db29
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2023-12-02 10:30:10 +0900

    add: docker group

--- add_user_groups.sh
+++ add_user_groups.sh
@@ -6,7 +6,7 @@ function main(){
         TARGET_USER="${1}"
     fi
     readonly TARGET_USER
-    local -r TARGET_GROUPS="adm dialout cdrom sudo video plugdev"
+    local -r TARGET_GROUPS="adm dialout docker cdrom sudo video plugdev"
     for g in ${TARGET_GROUPS}; do
         sudo gpasswd -a ${TARGET_USER} ${g}
     done
