34f36aa06a2e028deed9b0fc79c51dc5d5bfee6b
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2023-12-02 12:54:09 +0900

    add: pgm viewer extension

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -7,3 +7,4 @@ code --install-extension ms-python.python --force
 code --install-extension ms-vscode.cpptools --force
 code --install-extension shakram02.bash-beautify --force
 code --install-extension yzhang.markdown-all-in-one --force
+code --install-extension ngtystr.ppm-pgm-viewer-for-vscode --force
--- install_vscode_extensions_from_file.sh
+++ install_vscode_extensions_from_file.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 ./install_vscode_extension_from_file.sh DavidAnson vscode-markdownlint 0.51.0
 ./install_vscode_extension_from_file.sh DotJoshJohnson xml 2.5.1
 ./install_vscode_extension_from_file.sh mhutchie git-graph 1.30.0
@@ -7,3 +8,4 @@
 ./install_vscode_extension_from_file.sh ms-vscode cpptools 1.16.3
 ./install_vscode_extension_from_file.sh shakram02 bash-beautify 0.1.1
 ./install_vscode_extension_from_file.sh yzhang markdown-all-in-one 3.5.1
+./install_vscode_extension_from_file.sh ngtystr ppm-pgm-viewer-for-vscode 1.1.5
