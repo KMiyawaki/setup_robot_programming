44501c88b64426ecb15ac0dbbf73f4916602df19
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2025-10-17 17:21:50 +0900

    update: remove old key

--- ros1/install_ros1_key.sh
+++ ros1/install_ros1_key.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 function main(){
+    if [ -f /etc/apt/sources.list.d/ros-latest.list ]; then
+        sudo rm /etc/apt/sources.list.d/ros-latest.list
+    fi
     sudo apt-get install -y --no-install-recommends curl
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
     sudo curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+    if [ -f /etc/apt/sources.list.d/ros1-latest.list ]; then
+        sudo rm /etc/apt/sources.list.d/ros1-latest.list
+    fi    
 }
 
 main "$@"
